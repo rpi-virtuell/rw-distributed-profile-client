1797e3cb8f1e319b25c8ec55ade82878a9fc1721
Frank Staude <frank@staude.net>
2015-06-23 16:27:37 +0200

    add function_exists

--- inc/RW_Distributed_Profile_Client_Profile.php
+++ inc/RW_Distributed_Profile_Client_Profile.php
@@ -12,9 +12,13 @@ class RW_Distributed_Profile_Client_Profile {
      * @return null
      */
     public static function get_profile_list() {
+        if ( RW_Distributed_Profile_Client_Options::get_endpoint() == '' ) {
+            return false;
+        }
         $request = array(   'cmd' => 'list_profile' );
         $json = urlencode( json_encode( $request ) );
         $response = wp_remote_get( RW_Distributed_Profile_Client_Options::get_endpoint() . $json , array ( 'sslverify' => false ) );
+
         try {
             $json = json_decode( $response['body'] );
         } catch ( Exception $ex ) {
@@ -48,12 +52,14 @@ class RW_Distributed_Profile_Client_Profile {
             foreach( $json->message->wordpress as $key => $value ) {
                 update_user_meta( $user->ID, $key,$value );
             }
-            foreach( $json->message->buddypress as $group ) {
-                foreach( $group as $key => $value ) {
-                    $field = new BP_XProfile_Field( xprofile_get_field_id_from_name( $key ) );
-                    $fieldvalue = apply_filters( 'rw_distributed_profile_client_field_value', $value->value );
-                    $fieldvalue = apply_filters( 'rw_distributed_profile_client_field_'. $field->type . '_value', $fieldvalue );
-                    xprofile_set_field_data( $key, $user->ID, maybe_unserialize( $fieldvalue ) );
+            if ( function_exists( xprofile_get_field_id_from_name ) ) {
+                foreach( $json->message->buddypress as $group ) {
+                    foreach( $group as $key => $value ) {
+                        $field = new BP_XProfile_Field( xprofile_get_field_id_from_name( $key ) );
+                        $fieldvalue = apply_filters( 'rw_distributed_profile_client_field_value', $value->value );
+                        $fieldvalue = apply_filters( 'rw_distributed_profile_client_field_'. $field->type . '_value', $fieldvalue );
+                        xprofile_set_field_data( $key, $user->ID, maybe_unserialize( $fieldvalue ) );
+                    }
                 }
             }
         }
